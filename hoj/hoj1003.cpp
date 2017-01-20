c5d31dc72c93468d05ecd59406d19b4f23e68503
sysuKinthon <qinkai1994@qq.com>
2017-01-20 23:49:49 +0800

    update

---
+++ hoj/hoj1003.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, T,num, tmp;
+	int max_up, max_sum, max_down, cur_up, cur_sum, cur_down, pre_sum;
+	cin >> T;
+	for (t = 1; t <= T; t++)
+	{
+		// deal the input
+		cin >> num;
+		max_sum = cur_sum = -1001;
+		max_down = -1;
+		max_up = -1;
+		pre_sum = 0;
+
+		cur_down = 0;
+		cur_up = 0;
+		for(int i = 0; i < num; i++)
+		{
+			cin >> tmp;
+			if(tmp < 0 && cur_sum > max_sum)
+			{
+				max_sum = cur_sum;
+				max_down = cur_down;
+				max_up = cur_up;
+			}			
+			if(pre_sum >= 0)
+			{
+				cur_sum = pre_sum + tmp;
+				cur_up = i;
+			} else {
+				cur_sum = tmp;
+				cur_down = i;
+				cur_up = i;
+			}
+			pre_sum = cur_sum;
+		}
+		if(cur_sum > max_sum)
+		{
+			max_sum = cur_sum;
+			max_down = cur_down;
+			max_up = cur_up;
+		}
+		cout << "Case "<< t << ":" << endl;
+		cout << max_sum << " " << max_down+1 << " " << max_up+1 << endl;
+		if (t != T)
+		{
+			cout << endl;
+		}
+	}
+}
