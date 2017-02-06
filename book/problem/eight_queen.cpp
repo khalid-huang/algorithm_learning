440ff35e2531fd2cec168ae21892b738d9f1620d
sysuKinthon <qinkai1994@qq.com>
2017-02-06 21:37:45 +0800

    update

---
+++ book/problem/eight_queen.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void queen_solve(int num, int cur, int& rsl, int* C)
+{
+	cout << cur << "  " << num << endl;
+	if(cur == num)
+	{
+		rsl++;
+	} else {
+		for(int i = 0; i < num; i++)
+		{
+			C[cur] = i;
+			int ok = 1;
+			for(int j = 0; j < cur; j++)
+			{
+				if(C[cur] == C[j] || cur - C[cur] == j - C[j] || cur + C[cur] == j + C[j])
+				{
+					ok = 0;
+					break;
+				}
+			}
+			if(ok == 1)
+				queen_solve(num, cur+1, rsl, C);
+		}
+	}
+}
+
+int get_queen_solve(int num)
+{
+	int rsl = 0;
+	int C[num];
+	queen_solve(num , 0, rsl, C);
+	return rsl;
+}
+
+int main()
+{
+	int tot = get_queen_solve(8);
+	cout << tot << endl;
+}
--- utils/permutation.cpp
+++ utils/permutation.cpp
@@ -1,3 +1,4 @@
+//http://www.cnblogs.com/autosar/archive/2012/04/08/2437799.html
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -9,14 +10,15 @@ void swap(string& source, int first, int second)
 	source[second] = tmp;
 }
 
+//全排列
 void permutation(string letters, int start, int end, string cur, vector<string>& result)
 {
 	//cout << start << " " << end << endl;
-	if(start == end)
+	// 结束了
+	if(start > end) 
 	{
-		string tmp = cur + letters[start];
 		//cout << tmp<< endl;
-		result.push_back(tmp);
+		result.push_back(cur);
 		return;
 	} else {
 		for(int i = start; i <= end; i++)
@@ -37,6 +39,35 @@ vector<string> get_permutation(string letters)
 	return result;
 }
 
+//带有个数参数，比如从5个中先3个；算法的思想主要是将其分为两个集合，一个是已经选好的，从0到start, 另一个是选择集，从start到end；如果我们的计算已经到达了目标的话，就把目前的结果放入结果数组中；没有的话，我们要做的是将选择集里面的所有元素选择一次，将其与start进行交换（这次交换是进行纵向的扩展与对比，可以 自己画个示意图）
+void permutation1(string letters, int count, int start, int end, string cur, vector<string>& result, int target_count)
+{
+	//cout << start << " " << end << endl;
+	if(count == target_count)
+	{
+		//cout << tmp<< endl;
+		result.push_back(cur);
+		return;
+	} else {
+		for(int i = start; i <= end; i++)
+		{
+			swap(letters, i, start);
+			string tmp = cur + letters[start];
+			permutation1(letters, count + 1, start+1, end, tmp, result,target_count);
+			swap(letters, i, start);
+		}
+	}
+}
+
+vector<string> get_permutation1(string letters)
+{
+	std::vector<string> result;
+	int start =  0, end = letters.size() - 1;
+	int target_count = 2;
+	permutation1(letters,0, start, end, "", result, target_count);
+	return result;
+}
+
 void print_rsl(vector<string> result)
 {
 	int size = result.size();
@@ -48,7 +79,7 @@ void print_rsl(vector<string> result)
 
 int main()
 {
-	string letters("abc");
-	vector<string> result = get_permutation(letters);
+	string letters("0123456789");
+	vector<string> result = get_permutation1(letters);
 	print_rsl(result);
 }
